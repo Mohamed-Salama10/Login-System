05400123ed6cfd9fe6e40f508b039e16235cd174
Mohamed.Salama <mohamedsalamaaa@gmail.com>
2023-02-23 21:30:38 +0100

    adding validation parts

--- js/script.js
+++ js/script.js
@@ -45,7 +45,7 @@ function checkWarning() {
 }
 
 function createNewUser() {
-  if (checkWarning() == false) {
+  if (checkWarning() == false  && validation() ==true ) {
     user = {
       name: nameInput.value,
       password: passwordInput.value,
@@ -54,6 +54,52 @@ function createNewUser() {
     allUsers.push(user);
     localStorage.setItem("users", JSON.stringify(allUsers));
   }
+}
+
+function validation() {
+  flag = false;
+  if (userNameValidation(nameInput.value) == false) {
+    let userNameWarningMsg = document.getElementById("userNameWarningMsg");
+    userNameWarningMsg.innerHTML = `<div class="bg-danger rounded-2 text-center text-white mt-2">
+    User Name invalid
+  </div>`;
+    flag = false;
+  }
+
+  if (emailValidation(emailInput.value) == false) {
+    let emailWarning = document.getElementById("emailWarningMsg");
+    emailWarning.innerHTML = `<div class="bg-danger rounded-2 text-center text-white mt-2">
+    email invalid
+  </div>`;
+    flag = false;
+  }
+
+  if (passwordValidation(passwordInput.value) == false) {
+    let passwordWarningMsg = document.getElementById("passwordWarningMsg");
+    passwordWarningMsg.innerHTML = `<div class="bg-danger rounded-2 text-center text-white mt-2">
+    password invalid
+  </div>`;
+    console.log("dasdsadsadsadasd")
+    flag = false;
+  }
+
+  return true;
+}
+
+function userNameValidation(name) {
+  var nameValidation = /^[A-z0-9]{3,16}$/;
+  return nameValidation.test(name);
+}
+function emailValidation(email) {
+  var nameValidation =
+    /^[A-z0-9]{3,16}(.|_)[A-z0-9]{3,16}(.|_)[A-z0-9]{3,16}?(.|_)?@(gmail|yahoo|hotmail).com$/gm;
+  return nameValidation.test(email);
+}
+
+function passwordValidation(password) {
+  var nameValidation =
+  /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/gm;
+  return nameValidation.test(password);
 }
 
 function checkIfUserExist(userName) {
